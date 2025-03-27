4447b9f5eebc459de7965c859f5cb1188ac8d6ff
Kamil Janik <45401446+kamiljanik98@users.noreply.github.com>
2025-03-27 21:45:05 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -8,7 +8,5 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
-  // <Provider store={store}>  {/* Wrapping the App with Provider */}
-  // </Provider>
       <App />
 );
